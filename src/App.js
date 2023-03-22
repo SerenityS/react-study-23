15519665865680a4be25ccb1562d80e998a90f69
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-03-22 19:53:07 +0900

    feat: finish ex6 - event
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import './App.css';
 
 function Header(props) {
   return <header>
-    <h1><a href="">{props.title}</a></h1>
+    <h1><a href="/" onClick={
+      (event) => {
+        event.preventDefault();
+        props.onChangeMode();
+      }
+    }>{props.title}</a></h1>
   </header>
 }
 
@@ -10,7 +15,12 @@ function Nav(props) {
   const lis = [];
   for (let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
-    lis.push(<li><a href={'read/' + t.id}>{t.title}</a></li>)
+    lis.push(<li>
+      <a href={'read/' + t.id} onClick={event => {
+        event.preventDefault();
+        props.onChangeMode(t.id);
+      }}>{t.title}</a>
+    </li>)
   }
   return <nav>
     <ol>
@@ -34,10 +44,14 @@ function App() {
   ]
   return (
     <div>
-      <Header title="REACT"></Header>
-      <Nav topics={topics}></Nav>
+      <Header title="REACT" onChangeMode={() => {
+        alert('Header');
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id) => {
+        alert(id);
+      }}></Nav>
       <Article title="Welcome" body="Hello, WEB"></Article>
-    </div>
+    </div >
   );
 }
 
