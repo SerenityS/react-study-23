53cce8bbd194cadab2ae671e961b56d478f3501e
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-05-03 18:59:43 +0900

    feat: finish 2-4
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- react-router-dom/src/App.js
+++ react-router-dom/src/App.js
@@ -1,6 +1,49 @@
+import './index.css';
+import { Routes, Route, NavLink } from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      Home...
+    </div>
+  )
+}
+
+function Topics() {
+  return (
+    <div>
+      <h2>Topics</h2>
+      Topics...
+    </div>
+  )
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      Contact...
+    </div>
+  )
+}
+
 function App() {
   return (
-    <div>Hello React Router DOM</div>
+    <div>
+      <h2>Hello React Router DOM</h2>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/topics">Topics</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/topics" element={<Topics />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/*" element={'Not Found'}></Route>
+      </Routes>
+    </div>
   );
 }
 
