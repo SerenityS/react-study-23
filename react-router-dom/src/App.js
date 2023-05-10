c9a76b5d10dded9062910f849b79122911308334
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-05-10 14:57:47 +0900

    feat: finish 2-5
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- react-router-dom/src/App.js
+++ react-router-dom/src/App.js
@@ -1,5 +1,5 @@
 import './index.css';
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { Routes, Route, NavLink, useParams } from 'react-router-dom';
 
 function Home() {
   return (
@@ -10,11 +10,48 @@ function Home() {
   )
 }
 
+var contents = [
+  { id: 1, title: 'HTML', description: 'HTML is ...' },
+  { id: 2, title: 'JS', description: 'JS is ...' },
+  { id: 3, title: 'React', description: 'React is ...' },
+]
+
+function Topic() {
+  var params = useParams();
+  var topic_id = params.topic_id;
+  var selected_topic = {
+    title: 'Sorry',
+    description: 'Not Found'
+  };
+  for (var i = 0; i < contents.length; i++) {
+    if (contents[i].id === Number(topic_id)) {
+      selected_topic = contents[i];
+      break;
+    }
+  }
+  console.log(params);
+  return (
+    <div>
+      <h3>{selected_topic.title}</h3>
+      {selected_topic.description}
+    </div>
+  );
+}
+
 function Topics() {
+  var lis = [];
+  for (var i = 0; i < contents.length; i++) {
+    lis.push(<li key={contents[i].id}><NavLink to={'/topics/' + contents[i].id}>{contents[i].description}</NavLink></li>)
+  }
   return (
     <div>
       <h2>Topics</h2>
-      Topics...
+      <ul>
+        {lis}
+      </ul>
+      <Routes>
+        <Route path="/:topic_id" element={<Topic />} />
+      </Routes>
     </div>
   )
 }
@@ -39,7 +76,7 @@ function App() {
       </ul>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/topics" element={<Topics />}></Route>
+        <Route path="/topics/*" element={<Topics />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
         <Route path="/*" element={'Not Found'}></Route>
       </Routes>
